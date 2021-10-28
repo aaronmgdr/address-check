b038cc8f804090fccb56e5084fdeb998a549f780
Aaron <aaron@clabs.co>
2021-10-27 18:06:34 -0700

    Display the message nicely also display loading sign and store response with address so we dont display an old response when new address exists

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -8,30 +8,34 @@ export default function Demo() {
   const [address, setAddress] = useState("")
   const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setAddress(event.currentTarget.value)
-    if (event.currentTarget.value.length === 0){
-      setResponse({})
-    }
   }
+  const [isLoading, setLoading] = useState(false)
 
-  const [response, setResponse] = useState<Partial<AddressInfo>>({})
+  const [response, setResponse] = useState<Record<string,Partial<AddressInfo>>>({})
 
   const onSubmit: MouseEventHandler<HTMLButtonElement> = async (event) => {
     event.preventDefault()
     if (address.length === 0) {
       return
     }
+    setLoading(true)
     const res = await fetch(getUrl(address))
     const json = await res.json() as AddressInfo
-    setResponse(json)
+    setResponse({...response, [address]: json})
+    setLoading(false)
   }
+
+  const responseForAddress = response[address] || {}
 
   return <div className={styles.root}>
     <form className={styles.form}>
       <label>Enter an 0x Address</label>
       <div>
-      <input className={response.advice && styles[response.advice]} name="address" value={address} onChange={onChange} />
+      <input className={responseForAddress.advice && styles[responseForAddress.advice]} name="address" value={address} onChange={onChange} />
       <button onClick={onSubmit} >✔︎</button>
       </div>
+      {responseForAddress.message && <small className={`${styles.message} ${responseForAddress.advice && styles[responseForAddress.advice]}`}>{responseForAddress.message}</small>}
+      {isLoading && <small className={styles.message}>...Querying</small>}
     </form>
     <h5 className={styles.request}>Request</h5>
     <code className={styles.code}>
@@ -39,7 +43,7 @@ export default function Demo() {
     </code>
     <h5>Response</h5>
     <code className={styles.code}>
-      {JSON.stringify(response)}
+      {JSON.stringify(responseForAddress)}
     </code>
   </div>
 }
